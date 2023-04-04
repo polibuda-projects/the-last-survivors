5a709b3c9c907d13e7f4220bd05511dbaa28c26b
Daniel <daniel.z.7000000@gmail.com>
2023-04-04 19:04:27 +0200

    refactored Player Class and Player State class and added some animations to Player Class

--- core/src/com/polibudaprojects/thelastsurvivors/States/PlayState.java
+++ core/src/com/polibudaprojects/thelastsurvivors/States/PlayState.java
@@ -8,7 +8,6 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.polibudaprojects.thelastsurvivors.Player.DemoPlayer;
 import com.polibudaprojects.thelastsurvivors.Music.BackgroundMusic;
 import com.polibudaprojects.thelastsurvivors.monsters.MonsterFactory;
@@ -16,40 +15,40 @@ import com.polibudaprojects.thelastsurvivors.monsters.MonsterManager;
 import java.nio.file.Paths;
 
 public class PlayState extends State {
-    private ShapeRenderer shapeRenderer;
-    static private boolean projectionMatrixSet;
+    private final ShapeRenderer shapeRenderer;
     private final MonsterManager monsterManager;
-    Texture img;
     private final DemoPlayer demoPlayer;
-    private final Texture bg;
+    private final Texture backgroundPng;
 
-    Texture portrait;
+    private final Texture playerPortraitPng;
 
-    Texture rectangle1;
-
-    SpriteBatch batch;
-
-    TextureRegion region1;
-
-    TextureRegion region2;
+    private final TextureRegion playerStats;
 
     public PlayState(StatesManager gsm) {
+
         super(gsm);
-        batch = new SpriteBatch();
-        img = new Texture("player.png");
-        rectangle1 = new Texture("playerHub.png");
-        portrait = new Texture("portrait.png");
+
+        //Player Stats Background
+        Texture playerStatsPng = new Texture("playerHub.png");
+        playerStats = new TextureRegion(playerStatsPng, 0, 0, 300, 82);
+
+        //Player Portrait
+        playerPortraitPng = new Texture("portrait.png");
+
+        //Game Background
+        backgroundPng = new Texture("background.png");
+
+        //Used to generate HP Bar and Xp Bar
         shapeRenderer = new ShapeRenderer();
-        projectionMatrixSet = false;
-        region1 = new TextureRegion(img, 0, 0, 144, 80);
-        region2 = new TextureRegion(rectangle1, 0, 0, 300, 82);
+
         demoPlayer = new DemoPlayer();
         monsterManager = new MonsterManager(demoPlayer);
+
         cam.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        bg = new Texture("background.png");
+
         BackgroundMusic backgroundMusic = new BackgroundMusic(Paths.get("music/BackgroundTheLastSurvivors.mp3"));
+
         TiledMap tiledMap = new TmxMapLoader().load("example.tmx");
-        OrthogonalTiledMapRenderer tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
     }
 
     @Override
@@ -62,25 +61,30 @@ public class PlayState extends State {
         handleInput();
         demoPlayer.update(dt);
         monsterManager.update(dt);
-        if (demoPlayer.dead) {
+        if (demoPlayer.isGameOver()) {
             gsm.set(new EndState(gsm));
         }
     }
 
     @Override
     public void render(SpriteBatch sb) {
+
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
-        sb.draw(bg, cam.position.x - (cam.viewportWidth / 2), 0);
+        sb.draw(backgroundPng, cam.position.x - (cam.viewportWidth / 2), 0);
         demoPlayer.draw(sb);
         monsterManager.draw(sb);
-        sb.draw(region2,10,0);
-        sb.draw(portrait,25,27);
+        sb.draw(playerStats,10,0);
+        sb.draw(playerPortraitPng,25,27);
         sb.end();
+
+        //HP BAR
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
         shapeRenderer.setColor(Color.RED);
-        shapeRenderer.rect(82, 53, (195f*demoPlayer.currentHealth)/demoPlayer.maxHealth, 10);
+        shapeRenderer.rect(82, 53, (195f*demoPlayer.getCurrentHealth())/demoPlayer.getMaxHealth(), 10);
         shapeRenderer.end();
+
+        //XP BAR
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
         shapeRenderer.setColor(Color.SKY);
         shapeRenderer.rect(82, 34, 100, 10);
