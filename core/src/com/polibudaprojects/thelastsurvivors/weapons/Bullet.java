e820c38326199a85c4aab0a268e059cdcfcc777f
wojzam <wojciech.zamarski@student.pk.edu.pl>
2023-05-06 10:45:31 +0200

    fix bullet hitbox to match sprite

--- core/src/com/polibudaprojects/thelastsurvivors/weapons/Bullet.java
+++ core/src/com/polibudaprojects/thelastsurvivors/weapons/Bullet.java
@@ -9,26 +9,23 @@ public class Bullet {
     private final Vector2 position;
     private final Vector2 velocity;
     private final Sprite sprite;
-    private final Rectangle hitbox;
 
     public Bullet(Vector2 position, Vector2 velocity, Sprite sprite) {
         this.position = position;
         this.velocity = velocity;
         this.sprite = sprite;
-        hitbox = new Rectangle(position.x, position.y, 47, 33);
     }
 
     public void draw(SpriteBatch sb) {
-        sprite.setPosition(position.x, position.y);
         sprite.draw(sb);
     }
 
     public void update(float dt) {
         position.mulAdd(velocity, dt);
-        hitbox.setPosition(position);
+        sprite.setPosition(position.x, position.y);
     }
 
     public Rectangle getHitbox() {
-        return hitbox;
+        return sprite.getBoundingRectangle();
     }
 }
