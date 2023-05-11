f5658e96eeaaff5f9af599baa7fd755a098222ef
wojzam <wojciech.zamarski@student.pk.edu.pl>
2023-05-11 14:32:27 +0200

    improve visually loading screen

--- core/src/com/polibudaprojects/thelastsurvivors/States/LoadingState.java
+++ core/src/com/polibudaprojects/thelastsurvivors/States/LoadingState.java
@@ -9,14 +9,18 @@ import com.polibudaprojects.thelastsurvivors.Assets;
 
 public class LoadingState extends State {
     private static final String LOADING_TEXT = "Loading...";
-    private final ShapeRenderer shapeRenderer;
-    private final BitmapFont font;
+    private static final float DELAY = 0.2f;
+    private static final Color BAR_COLOR = Color.FIREBRICK;
+    private static final Color BAR_BACKGROUND_COLOR = new Color(0x302727ff);
+    private static final Color TOP_COLOR = Color.BLACK;
+    private static final Color BOTTOM_COLOR = new Color(0x133824ff);
+    private final ShapeRenderer shapeRenderer = new ShapeRenderer();
+    private final BitmapFont font = new BitmapFont();
+    private float delayTimer = 0;
     private float progress;
 
     protected LoadingState(StatesManager gsm) {
         super(gsm);
-        this.shapeRenderer = new ShapeRenderer();
-        this.font = new BitmapFont();
         this.font.setColor(Color.WHITE);
         this.font.getData().setScale(3f);
     }
@@ -32,25 +36,41 @@ public class LoadingState extends State {
     @Override
     public void update(float dt) {
         if (Assets.finishedLoading()) {
-            gsm.setState(gsm.getStart());
+            startAfterDelay(dt);
         }
         progress = Assets.getProgress();
-        progress = progress > 0.95f ? 1f : progress;
+    }
+
+    private void startAfterDelay(float dt) {
+        delayTimer += dt;
+        if (delayTimer > DELAY) {
+            gsm.setState(gsm.getStart());
+        }
     }
 
     @Override
     public void render(SpriteBatch sb) {
         shapeRenderer.setProjectionMatrix(cam.combined);
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        shapeRenderer.setColor(Color.GRAY);
+
+        shapeRenderer.rect(cam.position.x - (Gdx.graphics.getWidth() / 2f), cam.position.y - (Gdx.graphics.getHeight() / 2f), Gdx.graphics.getWidth(), Gdx.graphics.getHeight(),
+                BOTTOM_COLOR, BOTTOM_COLOR, TOP_COLOR, TOP_COLOR);
+
+        shapeRenderer.setColor(BAR_BACKGROUND_COLOR);
         shapeRenderer.rect(cam.position.x - (Gdx.graphics.getWidth() / 4f), cam.position.y, Gdx.graphics.getWidth() / 2f, 50);
-        shapeRenderer.setColor(Color.WHITE);
+        shapeRenderer.setColor(BAR_COLOR);
         shapeRenderer.rect(cam.position.x - (Gdx.graphics.getWidth() / 4f), cam.position.y, Gdx.graphics.getWidth() / 2f * progress, 50);
+
         shapeRenderer.end();
 
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
         font.draw(sb, LOADING_TEXT, cam.position.x - (Gdx.graphics.getWidth() / 4f), cam.position.y + 100);
+        font.draw(sb, getPercentProgressText(), cam.position.x + Gdx.graphics.getWidth() / 4f - 110, cam.position.y + 100);
         sb.end();
+    }
+
+    private String getPercentProgressText() {
+        return String.format("%3d%%", (int) (progress * 100));
     }
 }
