fca22a5128dc7cc2ac437f95e749959dda041f0e
wojzam <wojciech.zamarski@student.pk.edu.pl>
2023-05-11 17:04:30 +0200

    improve WeaponsList

--- core/src/com/polibudaprojects/thelastsurvivors/hud/WeaponsList.java
+++ core/src/com/polibudaprojects/thelastsurvivors/hud/WeaponsList.java
@@ -15,7 +15,9 @@ import java.util.stream.Collectors;
 
 public class WeaponsList implements HUD {
 
-    public static final float SIZE = 32f;
+    public static final float FRAME_SIZE = 32f;
+    public static final float ICON_SIZE = 30f;
+    private static final float MARGIN = (FRAME_SIZE - ICON_SIZE) / 2;
     private final ShapeRenderer shapeRenderer = new ShapeRenderer();
     private final Player player;
     private final Vector2 position = new Vector2();
@@ -27,27 +29,36 @@ public class WeaponsList implements HUD {
 
     @Override
     public void updatePosition(float cameraX, float cameraY) {
-        position.set(cameraX - Gdx.graphics.getWidth() / 2f, cameraY + Gdx.graphics.getHeight() / 2f - SIZE);
+        position.set(cameraX - Gdx.graphics.getWidth() / 2f, cameraY + Gdx.graphics.getHeight() / 2f - FRAME_SIZE);
     }
 
     @Override
     public void update(float dt) {
-        icons = player.getWeapons().stream().map(Weapon::getIcon).collect(Collectors.toList());
+        icons = player.getWeapons().stream()
+                .map(Weapon::getIcon)
+                .collect(Collectors.toList());
     }
 
     @Override
     public void render(SpriteBatch batch) {
-        batch.begin();
-        for (int i = 0; i < icons.size(); i++) {
-            batch.draw(icons.get(i), position.x + SIZE * i, position.y, SIZE, SIZE);
-        }
-        batch.end();
+        renderIcons(batch);
         shapeRenderer.setProjectionMatrix(batch.getProjectionMatrix());
         shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
-        shapeRenderer.setColor(Color.DARK_GRAY);
         for (int i = 0; i < Player.MAX_WEAPONS_COUNT; i++) {
-            shapeRenderer.rect(position.x + SIZE * i, position.y, SIZE, SIZE);
+            shapeRenderer.setColor(Color.DARK_GRAY);
+            shapeRenderer.rect(position.x + FRAME_SIZE * i, position.y, FRAME_SIZE, FRAME_SIZE);
+            shapeRenderer.rect(position.x + FRAME_SIZE * i + 2, position.y + 2, FRAME_SIZE - 4, FRAME_SIZE - 4);
+            shapeRenderer.setColor(Color.GRAY);
+            shapeRenderer.rect(position.x + FRAME_SIZE * i + 1, position.y + 1, FRAME_SIZE - 2, FRAME_SIZE - 2);
         }
         shapeRenderer.end();
+    }
+
+    private void renderIcons(SpriteBatch batch) {
+        batch.begin();
+        for (int i = 0; i < icons.size(); i++) {
+            batch.draw(icons.get(i), position.x + MARGIN + FRAME_SIZE * i, position.y + MARGIN, ICON_SIZE, ICON_SIZE);
+        }
+        batch.end();
     }
 }
