a79634592b56d2e64667562f2d8da0121c8a1aa4
wojzam <wojciech.zamarski@student.pk.edu.pl>
2023-05-15 10:49:49 +0200

    improve visually ChampionSelectState

--- core/src/com/polibudaprojects/thelastsurvivors/States/ChampionSelectState.java
+++ core/src/com/polibudaprojects/thelastsurvivors/States/ChampionSelectState.java
@@ -1,14 +1,16 @@
 package com.polibudaprojects.thelastsurvivors.States;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.polibudaprojects.thelastsurvivors.Assets;
@@ -17,29 +19,38 @@ import com.polibudaprojects.thelastsurvivors.Player.MageWarrior;
 import com.polibudaprojects.thelastsurvivors.Player.NightWarrior;
 
 public class ChampionSelectState extends State {
+
     private final Texture background;
-
     private final Stage stage;
-
-    Drawable drawable;
-    ImageButton mageButton;
-    ImageButton fireButton;
-    ImageButton nightButton;
-
+    private final ImageButton mageButton;
+    private final BitmapFont font = new BitmapFont();
+    private final ImageButton fireButton;
+    private final ImageButton nightButton;
+    private final GlyphLayout textLayout;
 
     public ChampionSelectState(StatesManager gsm) {
         super(gsm);
         background = Assets.get("background.png", Texture.class);
-        Texture mage = Assets.get("players/MageWarriorIcon.png", Texture.class);
-        Texture fireWarrior = Assets.get("players/FireWarriorIcon.png", Texture.class);
-        Texture nightWarrior = Assets.get("players/NightWarriorIcon.png", Texture.class);
-        drawable = new TextureRegionDrawable(new TextureRegion(mage));
-        mageButton = new ImageButton(drawable);
-        drawable = new TextureRegionDrawable(new TextureRegion(fireWarrior));
-        fireButton = new ImageButton(drawable);
-        drawable = new TextureRegionDrawable(new TextureRegion(nightWarrior));
-        nightButton = new ImageButton(drawable);
+        mageButton = new ImageButton(getDrawable("players/MageWarriorIcon.png"));
+        fireButton = new ImageButton(getDrawable("players/FireWarriorIcon.png"));
+        nightButton = new ImageButton(getDrawable("players/NightWarriorIcon.png"));
+        mageButton.setPosition(cam.position.x - mageButton.getWidth() / 2f - 300, cam.position.y - mageButton.getHeight() / 2f);
+        fireButton.setPosition(cam.position.x - fireButton.getWidth() / 2f, cam.position.y - fireButton.getHeight() / 2f);
+        nightButton.setPosition(cam.position.x - nightButton.getWidth() / 2f + 320, cam.position.y - nightButton.getHeight() / 2f);
+
         stage = new Stage(new ScreenViewport(cam));
+        stage.addActor(mageButton);
+        stage.addActor(fireButton);
+        stage.addActor(nightButton);
+        Gdx.input.setInputProcessor(stage);
+
+        font.setColor(Color.WHITE);
+        font.getData().setScale(3f);
+        textLayout = new GlyphLayout(font, "SELECT HERO");
+    }
+
+    private static TextureRegionDrawable getDrawable(String filename) {
+        return new TextureRegionDrawable(new TextureRegion(Assets.get(filename, Texture.class)));
     }
 
     @Override
@@ -62,13 +73,6 @@ public class ChampionSelectState extends State {
                 gsm.setState(gsm.getPlay());
             }
         });
-        mageButton.setPosition(cam.position.x - mageButton.getWidth() / 2f - 300, cam.position.y - mageButton.getHeight() / 2f);
-        fireButton.setPosition(cam.position.x - fireButton.getWidth() / 2f, cam.position.y - fireButton.getHeight() / 2f);
-        nightButton.setPosition(cam.position.x - nightButton.getWidth() / 2f + 320, cam.position.y - nightButton.getHeight() / 2f);
-        stage.addActor(mageButton);
-        stage.addActor(fireButton);
-        stage.addActor(nightButton);
-        Gdx.input.setInputProcessor(stage);
     }
 
     @Override
@@ -85,6 +89,7 @@ public class ChampionSelectState extends State {
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
         sb.draw(background, 0, 0);
+        font.draw(sb, textLayout, cam.position.x - (textLayout.width / 2), cam.position.y + 200);
         sb.end();
         stage.act(Gdx.graphics.getDeltaTime());
         stage.draw();
